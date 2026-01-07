19eb93d6a1516abeb8caa73314219ac598381ab2
sseungeun <iseungeun970@gmail.com>
2026-01-07 17:34:51 +0900

    260107 project

---
+++ turtleTest.py
@@ -0,0 +1,20 @@
+# from turtle import *
+# color('red', 'yellow')
+# begin_fill()
+# while True:
+#     forward(200)
+#     left(170)
+#     if abs(pos()) < 1:
+#         break
+# end_fill()
+# done()
+import turtle as t
+ 
+t2=t.Turtle()
+t2.shape('turtle')
+
+for i in range(4):
+    t2.forward(100)
+    t2.left(90)
+
+t.exitonclick()
